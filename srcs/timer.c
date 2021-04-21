8c9ba427150589b0af810eb392b8049964d1ccd2
Iipal <tmaluh@ustork.com.ua>
2021-04-21 04:42:53 +0300

    added output of stats at the end of time

--- srcs/timer.c
+++ srcs/timer.c
@@ -4,17 +4,25 @@ int __timer_seconds_counter = TIMER_SECONDS_DEFAULT;
 
 static inline void __timer_stop_typos(void) {
   const char *msg = "YOUR TIMED OUT";
+  struct s_stats stats = stats_get_all();
+  char stats_buffer[1024] = {0};
+  int y = print_line_input_get_y();
 
-  clear();
-  box(stdscr, 0, 0);
+  snprintf(stats_buffer, sizeof(stats_buffer), "WPM: %d; CPM: %d; TYPOS: %d;",
+           stats.words_per_minute, stats.characters_per_minute,
+           stats.total_miss_typos);
 
-  colorize_mvprintwe(COLORIZE_OK, 1, print_line_get_center_x(strlen(msg)), msg);
+  colorize_mvprintwe(COLORIZE_OK, y + 3, print_line_get_center_x(strlen(msg)),
+                     msg);
+  colorize_mvprintwe(COLORIZE_OK, y + 4,
+                     print_line_get_center_x(strlen(stats_buffer)),
+                     stats_buffer);
 
   int exit_input = 0;
   do {
     exit_input = typing_get_input();
-  } while (!(exit_input == TYPING_KEY_SPACE_BAR ||
-             exit_input == TYPING_KEY_NEW_LINE));
+  } while (
+      !(exit_input == TYPING_KEY_ESC || exit_input == TYPING_KEY_NEW_LINE));
 
   finish(0);
 }
@@ -34,7 +42,6 @@ static void __timer_handler(int signo) {
 inline void timer_init(int seconds) {
   if (TIMER_SECONDS_MIN <= seconds && TIMER_SECONDS_MAX >= seconds) {
     __timer_seconds_counter = seconds;
-    __timer_seconds_counter = 2;
   }
 
   struct sigaction act;
